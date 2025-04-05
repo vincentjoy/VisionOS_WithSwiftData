1859a953d701f08a9d0543ea498507c5c56fa6f2
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-05 13:59:02 +0530

    Created the project

---
+++ VisionOS_WithSwiftData/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionOS_WithSwiftData
+//
+//  Created by Vincent Joy on 05/04/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionOS_WithSwiftData/VisionOS_WithSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_WithSwiftDataApp.swift
+//  VisionOS_WithSwiftData
+//
+//  Created by Vincent Joy on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_WithSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
